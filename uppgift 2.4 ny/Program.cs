f8f26b84a080a1b4fd9a16c7025d94681a9fe065
hamadali06 <ahal06003@utb.vaxjo.se>
2023-09-15 11:23:26 +0200

    Add project files.

---
+++ uppgift 2.4 ny/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Uppgift_2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in lönen för arbetaren 1");
+            string x = Console.ReadLine();
+            int a = Convert.ToInt32(x);
+            Console.WriteLine("Skriv in lönen för arbetaren 2");
+            string y = Console.ReadLine();
+            int b = Convert.ToInt32(y);
+            Console.WriteLine("Skriv in lönen för arbetaren 3");
+            string z = Console.ReadLine();
+            int c = Convert.ToInt32(z);
+            int summa = a + b + c;
+            int medel = summa / 3;
+            Console.WriteLine("Medellönen för arbetarna är: " + medel);
+        }
+    }
+}
